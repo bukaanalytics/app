e941dc5596a36f3ad9463ae562200d54868fee0f
Aldi Doanta Kurnia <aldi@touchten.com>
2017-05-06 21:58:36 +0700

    Add initial sqlite wrapper, add Product and Stat model

---
+++ app/src/main/java/com/github/bukaanalytics/common/model/Product.java
@@ -0,0 +1,10 @@
+package com.github.bukaanalytics.common.model;
+
+/**
+ * Created by touchtenmac-19 on 5/6/17.
+ */
+
+public class Product {
+    public String id;
+    public String name;
+}
---
+++ app/src/main/java/com/github/bukaanalytics/common/model/Stat.java
@@ -0,0 +1,15 @@
+package com.github.bukaanalytics.common.model;
+
+import java.util.Date;
+
+/**
+ * Created by touchtenmac-19 on 5/6/17.
+ */
+
+public class Stat {
+    public Date date;
+    public String dayName;
+    public String productId;
+    public int viewCount;
+    public int totalViewCount;
+}
