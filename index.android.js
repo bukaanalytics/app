18d01b79ec5df98c7c97d2f42a3597abb40e8f2d
fawwaz <fawwazmuhammad@gmail.com>
2017-05-10 05:34:14 +0700

    Finished cherry-picking react-native experiment branch, please note that you need to modify build.gradle and settings.gradle since both files is ignored by .gitignore which generated by gitignore.io

--- index.android.js
+++ index.android.js
@@ -1,47 +1,12 @@
-'use strict';
+/**
+ * Load the App component.
+ *  (All the fun stuff happens in "/src/index.js")
+ *
+ * React Native Starter App
+ * https://github.com/mcnamee/react-native-starter-app
+ */
 
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import SQLite from 'react-native-sqlite-storage'
+import { AppRegistry } from 'react-native';
+import AppContainer from './src/';
 
-class HelloWorld extends React.Component {
-  
-  componentDidMount() {
-    let db = SQLite.openDatabase({name: 'baDatabase'}, () => {
-      // alert('connected')
-    })
-
-    db.executeSql("SELECT * FROM posts", [], (t, r) => {
-      // console.log(t.rows)
-      let result = t.rows.item(0)
-      console.log(result)
-    })
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.hello}>Hello, World</Text>
-	      <Text style={styles.hello}>This one came from RN</Text>
-      </View>
-    )
-  }
-}
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  hello: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
+AppRegistry.registerComponent('HelloWorld', () => AppContainer);
