430b9cd941e330f0e08a18c7d424bca5e93a0711
Habib Ridho <habibridho@live.com>
2017-05-11 00:09:30 +0700

    add overview section

--- js-src/screen/Home/HomeView.js
+++ js-src/screen/Home/HomeView.js
@@ -11,7 +11,8 @@ class Home extends Component {
   render() {
     return (
       <ScrollView style={AppStyles.flex1}>
-        <View style={[AppStyles.spreadHorizontalContainer, AppStyles.padding, {backgroundColor: AppColors.brand.lightPrimary}]}>
+        {/* Date Header */}
+        <View style={styles.dateHeaderContainer}>
           <View style={AppStyles.row}>
             <Icon name="date-range" size={36} />
             <View style={AppStyles.paddingLeft}>
@@ -28,8 +29,67 @@ class Home extends Component {
             </TouchableOpacity>
           </View>
         </View>
+        {/* End of Date Header */}
+
+        <Text style={styles.sectionTitle}>Overview</Text>
+        <View style={styles.overviewContainer}>
+          <View style={styles.statContainer}>
+            <Text style={styles.centeredH3}>Revenue</Text>
+            <Text style={styles.centeredStat}>400 K</Text>
+            <Text style={styles.statusTagOk}>Compared to Prev Period</Text>
+          </View>
+          <View style={styles.statContainer}>
+            <Text style={styles.centeredH3}>Conv. Rate</Text>
+            <Text style={styles.centeredStat}>3 %</Text>
+            <Text style={styles.statusTagWarning}>Compared to Prev Period</Text>
+          </View>
+        </View>
+
+        <Text style={styles.sectionTitle}>User Views by Day</Text>
       </ScrollView>
     )
+  }
+}
+
+const styles = {
+  centeredH3: {
+    ...AppStyles.textCenterAligned,
+    ...AppStyles.h3
+  },
+  centeredStat: {
+    ...AppStyles.textCenterAligned,
+    fontSize: 56
+  },
+  dateHeaderContainer: {
+    ...AppStyles.spreadHorizontalContainer,
+    ...AppStyles.padding, 
+    backgroundColor: AppColors.brand.lightPrimary
+  },
+  overviewContainer: {
+    ...AppStyles.row,
+    ...AppStyles.paddingHorizontal,
+    ...AppStyles.paddingVertical,
+    backgroundColor: AppColors.background
+  },
+  sectionTitle: {
+    ...AppStyles.h1,
+    ...AppStyles.padding
+  },
+  statContainer: {
+    ...AppStyles.flex1,
+    ...AppStyles.paddingHorizontal
+  },
+  statusTagOk: {
+    ...AppStyles.textCenterAligned,
+    backgroundColor: AppColors.brand.success,
+    padding: 4,
+    borderRadius: 16
+  },
+  statusTagWarning: {
+    ...AppStyles.textCenterAligned,
+    backgroundColor: AppColors.brand.accent,
+    padding: 4,
+    borderRadius: 16
   }
 }
 
