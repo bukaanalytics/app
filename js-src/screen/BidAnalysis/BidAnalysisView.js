6574761afcce48009fdc52d3d09c7e53c00d6d76
Habib Ridho <habibridho@live.com>
2017-05-12 10:28:01 +0700

    nav and screen skeleton

---
+++ js-src/screen/BidAnalysis/BidAnalysisView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button, Text, View } from 'react-native'
+
+class BidAnalysis extends Component{
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>BidAnalysis Screen</Text>
+      </View>
+    )
+  }
+}
+
+export default BidAnalysis
