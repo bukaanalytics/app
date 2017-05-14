c10886c349d434d62066d508a1a792df096f2acd
fawwazmuhammad@gmail.com <fawwazmuhammad@gmail.com>
2017-05-14 20:42:37 +0700

    Added Lodash as depedency, please run npm install Added Pricing Tools Added SubHeader Component

--- js-src/screen/PricingAnalysis/PricingAnalysisView.js
+++ js-src/screen/PricingAnalysis/PricingAnalysisView.js
@@ -1,14 +1,152 @@
-import React, { Component } from 'react'
-import { Button, Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, View, TouchableOpacity } from 'react-native';
+import SubHeader from '@ui/SubHeader';
+import { AppStyles } from '@theme/';
+import { Alerts, Button, Card, Spacer, Text } from '@components/ui/';
+import FormWrapper from 'tcomb-form-native';
+import _ from 'lodash';
+
+const FormWrapperStyle = _.cloneDeep(FormWrapper.form.Form.stylesheet);
+FormWrapperStyle.textbox.normal.backgroundColor = '#FFFFFF';
+FormWrapperStyle.textbox.error.backgroundColor = '#FFFFFF';
+FormWrapperStyle.textbox.normal.justifyContent = 'flex-start';
+FormWrapperStyle.textbox.normal.alignItems = 'center';
+FormWrapperStyle.textbox.normal.margin = 10;
+
+const FORM_FIELDS = FormWrapper.struct({
+  search: FormWrapper.String,
+});
+
+const FORM_OPTIONS = {
+  auto: 'placeholders',
+  stylesheet: FormWrapperStyle,
+};
 
 class PricingAnalysis extends Component{
+  static navigationOptions = {
+    title: 'Example',
+  };
+
+  constructor(props) {
+    super(props)
+  }
+
   render() {
+    const Form = FormWrapper.form.Form;
+
+
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>PricingAnalysis Screen</Text>
+      <View style={styles.container}>
+        <SubHeader style={styles.searchbar}>
+          <Form
+            ref={(f) => { this.form = f; }}
+            type={FORM_FIELDS}
+            options={FORM_OPTIONS}
+          />
+        </SubHeader>
+        <SubHeader>
+
+          <View style={styles.wrapperSubBar}>
+            <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={()=>{ console.log('somethng'); }}
+            >
+              <Text h1>Filter</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={()=>{ console.log('somethng'); }}
+            >
+              <Text h1>Category</Text>
+            </TouchableOpacity>
+          </View>
+
+        </SubHeader>
+        <ScrollView style={[AppStyles.container]}>
+
+          <View style={[AppStyles.paddingHorizontal]}>
+            <Spacer size={15} />
+            <Text h2>Pricing Analysis Tools</Text>
+          </View>
+
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={()=>{ console.log('somethng'); }}
+          >
+            <Card
+              image={{ uri: 'http://wp-api.mcnam.ee/wp-content/uploads/2016/10/brekkie-crumble-33651_l.jpeg' }}
+            >
+              <View style={[AppStyles.paddingLeftSml, AppStyles.paddingBottomSml]}>
+                <Text h3>Title of post</Text>
+                <Text>
+                  seharusnya ini diagram , nanti aing lagi cari librarynya
+                </Text>
+              </View>
+            </Card>
+          </TouchableOpacity>
+
+          <Card>
+            <View style={[AppStyles.paddingLeftSml, AppStyles.paddingBottomSml]}>
+              <View style={styles.wrapperText}>
+                <View style={styles.infoText}>
+                  <Text h4>Terendah</Text>
+                  <Text h3> 10K </Text>
+                </View>
+                <View style={styles.infoText}>
+                  <Text h4>Rata-Rata</Text>
+                  <Text h3> 20K </Text>
+                </View>
+                <View style={styles.infoText}>
+                  <Text h4>Termahal</Text>
+                  <Text h3> 30K </Text>
+                </View>
+              </View>
+
+              <View style={styles.infoText}>
+                <View style={styles.wrapperText}>
+                  <View style={styles.infoText}>
+                    <Text h2>Terbaik</Text>
+                    <Text h1> 20K </Text>
+                  </View>
+                </View>
+              </View>
+
+            </View>
+          </Card>
+
+        </ScrollView>
       </View>
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  searchbar: {
+    backgroundColor: '#C30F42',
+  },
+  wrapperText: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  infoText: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  wrapperSubBar: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  hello: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
 
 export default PricingAnalysis
