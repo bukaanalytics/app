0f3c1c126401ee3374233b2b06cc023a79da3968
Habib Ridho <habibridho@live.com>
2017-05-16 08:20:12 +0700

    add pie chart

--- js-src/screen/Home/HomeView.js
+++ js-src/screen/Home/HomeView.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Dimensions, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import { AppColors, AppSizes, AppStyles } from '@theme/'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import { Pie } from 'react-native-pathjs-charts'
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window')
 const viewStatWidth = (screenWidth-40)/7 // -40 karena paddingHorizontal
@@ -74,6 +75,35 @@ class Home extends Component {
             { viewStatDiameters.map(this.renderViewStat) }
           </View>
         </View>
+
+        <Text style={styles.sectionTitle}>Revenue Attribution</Text>
+        <View style={styles.revenueContainer}>
+          <Pie data={data}
+          options={options}
+          accessorKey="population"
+          margin={{top: 20, left: 20, right: 20, bottom: 20}}
+          color="#2980B9"
+          pallete={
+            [
+              {'r':25,'g':99,'b':201},
+              {'r':24,'g':175,'b':35},
+              {'r':190,'g':31,'b':69},
+              {'r':100,'g':36,'b':199},
+              {'r':214,'g':207,'b':32},
+              {'r':198,'g':84,'b':45}
+            ]
+          }
+          r={0}
+          R={150}
+          legendPosition="topLeft"
+          label={{
+            fontFamily: 'Arial',
+            fontSize: 8,
+            fontWeight: true,
+            color: '#ECF0F1'
+          }}
+          />
+        </View>
       </ScrollView>
     )
   }
@@ -81,6 +111,45 @@ class Home extends Component {
 
 // To be replaced by dynamic data
 const viewStat = [12, 15, 20, 8, 22, 30, 45]
+const data = [{
+      "name": "Washington",
+      "population": 7694980
+    }, {
+      "name": "Oregon",
+      "population": 2584160
+    }, {
+      "name": "Minnesota",
+      "population": 6590667,
+      "color": {'r':223,'g':154,'b':20}
+    }, {
+      "name": "Alaska",
+      "population": 7284698
+    }]
+const options = {
+      margin: {
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 20
+      },
+      width: 350,
+      height: 350,
+      color: '#2980B9',
+      r: 50,
+      R: 150,
+      legendPosition: 'topLeft',
+      animate: {
+        type: 'oneByOne',
+        duration: 200,
+        fillTransition: 3
+      },
+      label: {
+        fontFamily: 'Arial',
+        fontSize: 8,
+        fontWeight: true,
+        color: '#ECF0F1'
+      }
+    }
 
 const styles = {
   centeredH3: {
@@ -100,6 +169,11 @@ const styles = {
   overviewContainer: {
     ...AppStyles.row,
     ...AppStyles.paddingHorizontal,
+    ...AppStyles.paddingVertical,
+    backgroundColor: AppColors.background
+  },
+  revenueContainer: {
+    ...AppStyles.row,
     ...AppStyles.paddingVertical,
     backgroundColor: AppColors.background
   },
