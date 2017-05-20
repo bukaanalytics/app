812bd5abed10f23ed6fee7716d2958ff7f8d4a15
Habib Ridho <habibridho@live.com>
2017-05-20 14:41:36 +0700

    implement date range

--- js-src/redux/index.js
+++ js-src/redux/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 
+import dashboard from '@redux/dashboard/reducer';
 import nav from '@redux/nav/reducer';
 import user from '@redux/user/reducer';
 import pricing from '@redux/pricing/reducer';
 
 // Combine all
 const appReducer = combineReducers({
+  dashboard,
   nav,
   user,
   pricing,
